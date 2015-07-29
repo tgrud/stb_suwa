fece287f30330a0fe2245f559ae1cd168e441ea4
tgrud <tgrud@users.noreply.github.com>
2015-07-29 23:32:24 +0000

    Start form toggle button functionality. Give reviewForm directive a controller

--- app/js/app.js
+++ app/js/app.js
@@ -27,7 +27,11 @@ angular.module('readingList', [])
     return {
       restrict: 'E',
       templateUrl: 'partials/review-form.html',
-      replace: true
+      replace: true,
+      controller: function(){
+        this.showForm = false;
+      },
+      controllerAs: 'reviewformCtrl'
     }
   });
     
